00d7ba3d5eb3b88e600c1d7fd35085bd5cc3a54b
Florian Ragwitz <rafl@debian.org>
2009-06-30 06:02:15 +0200

    Version 0.09.

--- lib/Devel/PartialDump.pm
+++ lib/Devel/PartialDump.pm
@@ -8,7 +8,7 @@ use Scalar::Util qw(looks_like_number reftype blessed);
 
 use namespace::clean -except => 'meta';
 
-our $VERSION = "0.08";
+our $VERSION = "0.09";
 
 use Sub::Exporter -setup => {
 	exports => [qw(dump warn show show_scalar croak carp confess cluck $default_dumper)],
